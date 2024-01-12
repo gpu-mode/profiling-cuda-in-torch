5199ba0c63e8cac76d1c77dce00c5854a38f8b60
Mark Saroufim <marksaroufim@meta.com>
2024-01-12 00:30:08 +0000

    hello load inline

---
+++ hello_load_inline.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.cpp_extension import load_inline
+
+cpp_source = """
+std::string hello_world() {
+  return "Hello World!";
+}
+"""
+
+my_module = load_inline(
+    name='my_module',
+    cpp_sources=[cpp_source],
+    functions=['hello_world'],
+    verbose=True
+)
+
+print(my_module.hello_world())
