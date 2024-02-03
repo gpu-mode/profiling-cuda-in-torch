333ba64033f6fdcbd690bf47f3529f833e45e0c1
Mark Saroufim <marksaroufim@gmail.com>
2024-02-03 14:02:24 -0800

    remove more junk

--- square.cu
+++
@@ -1,9 +0,0 @@
-cuda_kernel = """
-extern "C" __global__
-void square_kernel(const float* __restrict__ input, float* __restrict__ output, int size) {
-    const int index = blockIdx.x * blockDim.x + threadIdx.x;
-    if (index < size) {
-        output[index] = input[index] * input[index];
-    }
-}
-"""
